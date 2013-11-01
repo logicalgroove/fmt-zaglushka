b3fcc974be230582ce26132dcaafefdf90a534f3
LogicalGroove <webodessa@gmail.com>
2013-11-01 13:42:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zaglushka::Application.config.secret_token = '49045b9c5d0f1ef65a2f5054a9bf21ebe3d8d2eb25632ec1938942955d6d8fa47e5bc4b41fd1f5bab1af1d1767a564a21898a3fb606fca6eb00b2e28cf3baa56'
