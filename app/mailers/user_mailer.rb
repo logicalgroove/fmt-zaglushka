391374b532ef6071ea3f39e052a1c283cc79730c
Gsowhat <dr_joss@mail.ru>
2014-01-09 05:53:57 -0600

    Deleted some gaps
    [#62791066]

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,16 +3,16 @@ class UserMailer < ActionMailer::Base
   layout 'mailer_default'
 
   def registration_confirmation(user)
-      @user = user
-      mail(:to => user.email, :subject => "Добро пожаловать!")
+    @user = user
+    mail(:to => user.email, :subject => "Добро пожаловать!")
   end
 
 
  class Preview < MailView
-    def user_email
-      user = User.first
-      mail = UserMailer.registration_confirmation(user)
-      mail
-    end
+   def user_email
+     user = User.first
+     mail = UserMailer.registration_confirmation(user)
+     mail
+   end
   end
 end
