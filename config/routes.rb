a44ec4821eef513e83afede93b6f26017bcacac0
Gsowhat <dr_joss@mail.ru>
2014-02-06 10:25:48 -0600

    Refactoring error pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Zaglushka::Application.routes.draw do
 
   root :to => 'welcome#index'
   match 's/:id' => 'shorts#show', :as => 's_id'
-  match '/404' => 'errors#not_found'
-  match '/422' => 'errors#server_error'
-  match '/500' => 'errors#server_error'
   resources :users
   resources :cities
   namespace :admin do
