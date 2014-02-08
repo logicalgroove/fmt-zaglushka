cc06c785dba2112e6dd808b89a8ff29506ca8a41
logicalgroove <info@logicalgroove.com>
2014-02-08 16:59:48 +0000

    Users with short link can edit their map

--- app/controllers/shorts_controller.rb
+++ app/controllers/shorts_controller.rb
@@ -4,6 +4,7 @@ class ShortsController < ApplicationController
   def show
     user = User.where(:short_id => short).first
     respond_to do |format|
+      session[:user_id] = user.id
       format.html { redirect_to user_path(user) }
     end
   end
